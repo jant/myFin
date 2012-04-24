b3994ce2e1e33105adb2156fb4d97a860c7e119a
jant <tjanousek@volny.cz>
2012-04-24 23:49:09 +0200

    roky do hlavicky tabulky

--- rails/myfin/app/helpers/application_helper.rb
+++ rails/myfin/app/helpers/application_helper.rb
@@ -10,4 +10,19 @@ module ApplicationHelper
     ret_arr
   end
 
+  def year_ends(d_from, d_to=Date.current)
+    d = d_from.end_of_year
+    ret_arr = [] << d
+    while d < d_to
+      d >>= 12
+      ret_arr << d
+    end
+    ret_arr
+  end
+
+  def months_in_year(months, year)
+    sel = months.select {|d| d <= year.end_of_year && d >= year.beginning_of_year}
+    sel.count
+  end
+
 end
